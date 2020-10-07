e19d0d32966ceb256ba23637316018e8c940c278
general-rishkin <gennadiy.rishkin@gmail.com>
2020-10-07 12:44:30 +0100

    Added tests for the condition_variable implementation

--- Mutlithreading/condition_variable.zig
+++ Mutlithreading/condition_variable.zig
@@ -4,21 +4,21 @@ const std = @import("std");
 usingnamespace @import("semaphore.zig");
 
 pub const Lock = struct {
-    const Self: @This();
+    const Self = @This();
 
     inner_mutex: std.Mutex = std.Mutex{},
 
     pub fn acquire(self: *Self) void {
-        _ = self.inner.acquire();
+        _ = self.inner_mutex.acquire();
     }
 
     pub fn release(self: *Self) void {
-        (std.Mutex.Held{ .mutex = &self.inner }).release();
+        (std.Mutex.Held{ .mutex = &self.inner_mutex }).release();
     }
 };
 
 pub const ConditionVariable = struct {
-    const Self: @This();
+    const Self = @This();
 
     head: ?*Waiter = null,
 
@@ -94,7 +94,7 @@ pub const ConditionVariable = struct {
         return waiter;
     }
 
-    fn remove(self: *self, waiter: *Waiter) void {
+    fn remove(self: *Self, waiter: *Waiter) void {
         const head = self.head orelse return;
 
         if (waiter.prev) |prev|
@@ -116,27 +116,88 @@ pub const ConditionVariable = struct {
     }
 };
 
-test "Condition Variable" {
-    const Worker = struct {
-        sem: Semaphore = Semaphore{},
+test "C++'s condition_variable::notify_one" {
+    // http://www.cplusplus.com/reference/condition_variable/condition_variable/notify_one/
+    
+    const ThreadContext = struct {
+        const Self = @This();
 
-        fn run(self: *@This()) void {
-            std.time.sleep(1 * std.time.ns_per_s);
-            std.debug.print("[2] Waiting for signal", .{});
-            self.sem.wait();
-            std.debug.print("[3] Signalling", .{});
-            _ = self.sem.signal();
-            std.debug.print("[4] Signalled", .{});
+        cargo: u32 = 0,
+        cv_produce: ConditionVariable = ConditionVariable{},
+        cv_consume: ConditionVariable = ConditionVariable{},
+
+        fn consumer(self: *Self) void {
+            var lock = Lock{};
+            while (0 == self.cargo) self.cv_consume.wait(&lock);
+            std.debug.print("Thread {} => consuming => {}\n", .{std.Thread.getCurrentId(), self.cargo});
+            self.cargo = 0;
+            self.cv_produce.signal();
+        }
+
+        fn producer(self: *Self) void {
+            var lock = Lock{};
+            while (0 != self.cargo) self.cv_produce.wait(&lock);
+            self.cargo = std.Thread.getCurrentId();
+            self.cv_consume.signal();
         }
     };
 
-    var worker = Worker{};
+    std.debug.print("\n\n", .{});
+
+    var thread_context = ThreadContext{};
     
-    var t = try std.Thread.spawn(&worker, Worker.run);
+    const thread_count = 11;
+    var consumers: [thread_count]*std.Thread = undefined;
+    var producers: [thread_count]*std.Thread = undefined;
 
-    std.debug.print("[1] Waiting 4 seconds", .{});
-    std.time.sleep(4 * std.time.ns_per_s);
+    for (consumers) |*cs| {
+        cs.* = try std.Thread.spawn(&thread_context, ThreadContext.consumer);
+    }
 
-    t.wait();
-    std.debug.print("[5] main thread done", .{});
+    for (producers) |*pd| {
+        pd.* = try std.Thread.spawn(&thread_context, ThreadContext.producer);
+    }
+
+    for (consumers) |cs|
+        cs.wait();
+
+    for (producers) |pd|
+        pd.wait();
+}
+
+test "C++'s condition_variable::notify_all" {
+    // http://www.cplusplus.com/reference/condition_variable/condition_variable/notify_all/
+
+    const ThreadContext = struct {
+        const Self = @This();
+
+        ready: bool                             = false,
+        condition_variable: ConditionVariable   = ConditionVariable{},
+
+        fn print_id(self: *Self) void {
+            var lock = Lock{};
+            while (false == self.ready) self.condition_variable.wait(&lock);
+            std.debug.print("Thread ID = {}\n", .{std.Thread.getCurrentId()});
+        }
+
+        fn go(self: *Self) void {
+            var lock = Lock{};
+            self.ready = true;
+            self.condition_variable.broadcast();
+        }
+    };
+
+    std.debug.print("\n\n", .{});
+    var thread_context = ThreadContext{};
+    
+    const thread_count = 11;
+    var threads: [thread_count]*std.Thread = undefined;
+
+    for (threads) |*thread| {
+        thread.* = try std.Thread.spawn(&thread_context, ThreadContext.print_id);
+    }
+
+    thread_context.go();
+    for (threads) |thread|
+        thread.wait();
 }
